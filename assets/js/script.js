47e5210800da1118f54072ab80fe7f9b1d750e2f
aisha-ysn <aishayasin1801@gmail.com>
2021-08-08 18:59:16 +0100

    updated scrip.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,14 +15,16 @@ function initPage() {
 
 
     const APIKey = "c9a9ed03a355403f4cb9a36e931c0b4a";
-
+    //  When search button is clicked, read the city name typed by the user
 
     function getWeather(cityName) {
-
+        //  Using saved city name, execute a current condition get request from open weather map api
         let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
         axios.get(queryURL)
             .then(function(response) {
                 console.log(response);
+                //  Parse response to display current conditions
+                //  Method for using "date" objects obtained from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
                 const currentDate = new Date(response.data.dt * 1000);
                 console.log(currentDate);
                 const day = currentDate.getDate();
@@ -46,11 +48,12 @@ function initPage() {
                         currentUVEl.innerHTML = "UV Index: ";
                         currentUVEl.append(UVIndex);
                     });
-
+                //  Using saved city name, execute a 5-day forecast get request from open weather map api
                 let cityID = response.data.id;
                 let forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?id=" + cityID + "&appid=" + APIKey;
                 axios.get(forecastQueryURL)
                     .then(function(response) {
+                        //  Parse response to display forecast for next 5 days underneath current conditions
                         console.log(response);
                         const forecastEls = document.querySelectorAll(".forecast");
                         for (i = 0; i < forecastEls.length; i++) {
@@ -100,6 +103,7 @@ function initPage() {
         historyEl.innerHTML = "";
         for (let i = 0; i < searchHistory.length; i++) {
             const historyItem = document.createElement("input");
+            // <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="email@example.com"></input>
             historyItem.setAttribute("type", "text");
             historyItem.setAttribute("readonly", true);
             historyItem.setAttribute("class", "form-control d-block bg-white");
@@ -117,7 +121,7 @@ function initPage() {
     }
 
 
-    //  Saves search requests and display them underneath search form
+    //  Save user's search requests and display them underneath search form
     //  When page loads, automatically generate current conditions and 5-day forecast for the last city the user searched for
 
 }
